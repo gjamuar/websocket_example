8450e02bfc34a5cc31365f2bfb60e631ac8f93b6
jamuar <jamuar@adobe.com>
2020-06-01 15:10:04 -0400

    increased file upload limit to 1gb

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ app = Flask(__name__)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['FINGERPRINT_FOLDER'] = FINGERPRINT_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024
 cors = CORS(app)
